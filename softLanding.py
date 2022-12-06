877dd9cb242ae2011d5c724ab7ac6627065d78d2
stella <stellawaihuini@gmail.com>
2022-12-06 05:34:08 +0000

    payments set

--- softLanding.py
+++ softLanding.py
@@ -14,6 +14,22 @@ else :
    
     print("Incorrect username , please enter the details again")
 
+payment = ["cash" ,"card","mobile money"]
 
+print(f"Available payment options" )
+print(payment)
+
+modeOfPayment = input("enter prefered mode of payment: ")
+
+if modeOfPayment in payment:
+    print ("Proceed to checkout")
+else:
+    print(f"You entered" + " " +  modeOfPayment + " " + "not accepted here, please enter any of the 3 available options")
+    print(payment)
+
+ 
+
+   
+    
 
 
